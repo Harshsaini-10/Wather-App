fad57cf19c247ac3a482e92b2b0fb7e8d844c0eb
Harsh saini <harsh10ten@gmail.com>
2025-08-06 15:32:13 +0530

    wather app

---
+++ weather.js
@@ -0,0 +1,57 @@
+const apiKey = "ef9de073a434254cdca8f309ea3093c5";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    try {
+        const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
+
+        if (!response.ok) {
+            throw new Error("worng city name");
+        }
+
+        var data = await response.json();
+
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "clouds.png";
+        }
+        else if (data.weather[0].main == "Clear"){
+            weatherIcon.src = "clear.png";
+        }
+        else if (data.weather[0].main == "Rain"){
+            weatherIcon.src = "rain.png";
+        }
+        else if (data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "drizzle.png";
+        }
+        else if (data.weather[0].main == "Mist"){
+            weatherIcon.src = "mist.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+// एंटर की दबाने पर भी सर्च हो
+searchBox.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
+});
